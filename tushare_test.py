ea8f02c26db1555330f28089acdd25f86b11699c
wangted007 <wangted007@163.com>
2020-07-09 20:40:41 +0800

    rename file name

--- tushare_test.py
+++ tushare_test.py
@@ -1,13 +1,15 @@
 import tushare as ts
 
-#ts.set_token("ca9a880eb55cf5479fe8b021615f26d1a59c6f187e9d008a817c4ae0")
+ts.set_token("ca9a880eb55cf5479fe8b021615f26d1a59c6f187e9d008a817c4ae0")
+#print(dir(ts))
 def history_quote():
-    api =  ts.pro_api("ca9a880eb55cf5479fe8b021615f26d1a59c6f187e9d008a817c4ae0")
+    api =  ts.pro_api()
     klines = api.daily(ts_code='000001.SZ', start_date='2019-01-01')
     print(klines)
 
 def main():
-    history_quote()
+    pass
+    #history_quote()
 
 if __name__ == '__main__':
     main()
